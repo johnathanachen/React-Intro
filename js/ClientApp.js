f1e14ac4138e12bcc315c481ec0cebffd484f41d
jc.swift <jc.swift@icloud.com>
2017-06-21 01:37:11 -0500

    bundling webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,30 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyTitle from './MyTitle'
+
 var div = React.DOM.div
-var h1 = React.DOM.h1
 
-var myTitle = React.createClass({
-    render: function () {
-        return (
-            div(null,
-            h1({ style: { color: this.props.color } }, this.props.title)
-            )
-        )
-    }
-})
-
-var MyTitleFactory = React.createFactory(myTitle)
+var MyTitleFactory = React.createFactory(MyTitle)
 
 var myFirstComponent = React.createClass({
-            render: function () {
-                return (
-                    div(null, 
+  render: function () {
+    return (
+                    div(null,
                         MyTitleFactory({ title: 'props are the best', color: 'red' }),
-                        MyTitleFactory({ title: 'semicolons are the worst', color: 'blue'  }),
-                        MyTitleFactory({ title: 'jklol its ok i like semicolons', color: 'green'  }),
-                        MyTitleFactory({ title: 'haha i like to watch youtube', color: 'yellow'  })
+                        MyTitleFactory({ title: 'semicolons are the worst', color: 'blue' }),
+                        MyTitleFactory({ title: 'jklol its ok i like semicolons', color: 'green' }),
+                        MyTitleFactory({ title: 'haha i like to watch youtube', color: 'yellow' })
                     )
-                )
-            }
-        })
+    )
+  }
+})
 
-        ReactDOM.render(React.createElement(myFirstComponent), document
+ReactDOM.render(React.createElement(myFirstComponent), document
         .getElementById('app'))
