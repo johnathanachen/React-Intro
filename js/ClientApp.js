59186055da728ef1bfc31b4ee44630c718fd698c
jc.swift <jc.swift@icloud.com>
2017-06-21 23:15:12 -0500

    configuring css imports

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,19 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var div = React.DOM.div
-
-var MyTitleFactory = React.createFactory(MyTitle)
-
 var myFirstComponent = React.createClass({
   render: function () {
     return (
-                    div(null,
-                        MyTitleFactory({ title: 'props are the best', color: 'red' }),
-                        MyTitleFactory({ title: 'semicolons are the worst', color: 'blue' }),
-                        MyTitleFactory({ title: 'jklol its ok i like semicolons', color: 'green' }),
-                        MyTitleFactory({ title: 'haha i like to watch youtube', color: 'yellow' })
-                    )
+      <div>
+      <MyTitle title='props are cool' color='rebeccapurple'/>
+      <MyTitle title='props are amazing' color='mediumaquarium'/>
+      <MyTitle title='props are wonderful' color='blue'/>
+      </div>
     )
   }
 })
