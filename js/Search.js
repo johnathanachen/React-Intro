f9947870bcbee9fdfaebcc80c912df27967b52aa
jc.swift <jc.swift@icloud.com>
2017-06-22 01:57:08 -0500

    iterating through data

--- js/Search.js
+++ js/Search.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import preload from '../public/data.json'
 
 const Search = React.createClass({
-    render() {
-        return (
-            <h1>Search Page!</h1>
-        )
-    }
+  render () {
+    return (
+      <div className='search'>
+        {preload.shows.map((show) => {
+          return (
+            <h1>{show.title}</h1>
+
+            )
+        })}
+      </div>
+    )
+  }
 })
 
 export default Search
