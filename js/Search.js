3ce152d46d0eb10f53896edc6d460b88476854d7
jc.swift <jc.swift@icloud.com>
2017-06-22 02:45:20 -0500

    created showcard component

--- js/Search.js
+++ js/Search.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ShowCard from './ShowCard'
 import preload from '../public/data.json'
 
 const Search = React.createClass({
@@ -7,8 +8,7 @@ const Search = React.createClass({
       <div className='search'>
         {preload.shows.map((show) => {
           return (
-            <h1>{show.title}</h1>
-
+            <ShowCard show={show} />
             )
         })}
       </div>
