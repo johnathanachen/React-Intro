3e756e2a7dc9c3868d4da79cae64e4ed475284b4
Johnathan Chen <jc.swfit@icloud.com>
2017-06-23 17:57:06 -0500

    adding redux to application

--- js/Landing.js
+++ js/Landing.js
@@ -1,17 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
+const { string } = React.PropTypes
 
 const Landing = React.createClass({
+  propTypes: {
+    searchTerm: string
+  },
   render () {
     return (
       <div className='landing'>
         <img className='logo' src='../public/img/logo.png' />
-        <input type='text' placeholder='Search movies' />
+        <input value={this.props.searchTerm} type='text' placeholder='Search movies' />
         <Link to='/search'>Browse All</Link>
       </div>
     )
   }
 })
 
-export default Landing
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
 
+export default connect(mapStateToProps)(Landing)
